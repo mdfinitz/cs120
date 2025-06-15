497e7fb8ec284f1dbdd72aa18b644b73e1f0a1d0
mdfinitz <mfinit02@tufts.edu>
2025-06-15 03:01:06 -0400

    Delete nav.js

--- nav.js
+++
@@ -1,8 +0,0 @@
-document.write("<ul>");
-    document.write("<li><a href='project1_home.html'>HOME</a></li>");
-    document.write("<li><a href='project1_book.html'>BOOK</a></li>");
-    document.write("<li><a href='project1_songs.html'>SONGS</a></li>");
-    document.write("<li><a href='project1_home.html'><img src='images/logo.png'></a></li>");
-    document.write("<li><a href='project1_merchandise.html'>MERCHANDISE</a></li>");
-    document.write("<li><a href='project1_faq.html'>FAQ</a></li>");
-document.write("</ul>");
